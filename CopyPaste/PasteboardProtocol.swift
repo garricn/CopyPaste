2879c7ebfb6ca66ced6c2fd7ed27f72bc98eba1d
Garric G. Nahapetian <garricn@icloud.com>
2017-05-25 09:52:45 -0700

    Refactor TableViewModelTestCase

---
+++ CopyPaste/PasteboardProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  PasteboardProtocol.swift
+//  CopyPaste
+//
+//  Created by Garric G. Nahapetian on 5/24/17.
+//  Copyright © 2017 SwiftCoders. All rights reserved.
+//
+
+import UIKit
+
+protocol PasteboardProtocol: class {
+    var string: String? { get set }
+}
