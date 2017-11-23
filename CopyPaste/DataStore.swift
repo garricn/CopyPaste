06f4c44db31e35d9f046e4ea68b47496d6609380
Garric Nahapetian <garricn@icloud.com>
2017-11-22 20:12:37 -0800

    Add DataStore file

---
+++ CopyPaste/DataStore.swift
@@ -0,0 +1,70 @@
+//
+//  DataStore.swift
+//  CopyPaste
+//
+//  Created by Garric G. Nahapetian on 11/22/17.
+//  Copyright © 2017 SwiftCoders. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+final class DataStore {
+    
+    private let encoder: JSONEncoding
+    private let decoder: JSONDecoding
+    private let location: URL
+    
+    init(encoder: JSONEncoding, decoder: JSONDecoding, location: URL) {
+        self.encoder = encoder
+        self.decoder = decoder
+        self.location = location
+    }
+    
+    func encode<E: Encodable>(_ encodable: E) {
+        do {
+            try encoder.encode(encodable.self).write(to: location.appendingPathComponent(E.pathComponent))
+        } catch {
+            fatalError("\(error)")
+        }
+    }
+    
+    func decode<D: Decodable>(_ type: D.Type) -> D {
+        do {
+            let data = try Data(contentsOf: location.appendingPathComponent(D.pathComponent))
+            let decodable = try decoder.decode(type, from: data)
+            return decodable
+        } catch {
+            fatalError("\(error)")
+        }
+    }
+    
+    func json<E: Encodable>(from encodable: E) throws -> String {
+        let data = try encoder.encode(encodable)
+        return String(data: data, encoding: .utf8) ?? ""
+    }
+}
+
+protocol JSONEncoding {
+    func encode<T>(_ value: T) throws -> Data where T : Encodable
+}
+
+protocol JSONDecoding {
+    func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable
+}
+
+extension JSONEncoder: JSONEncoding {}
+extension JSONDecoder: JSONDecoding {}
+
+private extension Encodable {
+    static var pathComponent: String {
+        return String.init(describing: self)
+    }
+}
+
+private extension Decodable {
+    static var pathComponent: String {
+        return String.init(describing: self)
+    }
+}
