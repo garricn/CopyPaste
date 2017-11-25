5671aebf8de38931bfb50fb5a69ad0e404603513
Garric Nahapetian <garricn@icloud.com>
2017-11-24 17:47:58 -0800

    Refactor using Flow and closure pattern

--- CopyPaste/TableViewController.swift
+++ CopyPaste/TableViewController.swift
@@ -6,13 +6,24 @@ import UIKit
 
 final class TableViewController: UIViewController {
 
-    private let tableView = TableView(frame: CGRect.zero, style: .plain)
-    private let viewModel: TableViewModeling
+    var viewModel: TableViewModel {
+        didSet {
+            didSetViewModel()
+        }
+    }
+
+    private let tableView: TableView
+
+    private var didTap: ((UIBarButtonItem) -> Void)?
+    private var didSelectRow: ((IndexPath, UITableView) -> Void)?
+    private var didLongPressRow: ((IndexPath, UITableView) -> Void)?
+    private var didCommitEditing: ((UITableViewCellEditingStyle, IndexPath, UITableView) -> Void)?
 
     // MARK: - Life Cycle
 
-    init(viewModel: TableViewModeling) {
+    init(viewModel: TableViewModel, tableView: TableView) {
         self.viewModel = viewModel
+        self.tableView = tableView
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -29,14 +40,39 @@ final class TableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureNavigationItems()
         configureTableView()
-        configureViewModel()
-        viewModel.viewDidLoad() // Must be called after ViewModel configured
+        didSetViewModel()
     }
 
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
         tableView.setEditing(editing, animated: animated)
+    }
+
+    func onDidTapAddBarButtonItem(perform action: @escaping ((UIBarButtonItem) -> Void)) {
+        didTap = action
+    }
+    
+    func onDidSelectRow(perform action: @escaping ((IndexPath, UITableView) -> Void)) {
+        didSelectRow = action
+    }
+    
+    func onDidLongPress(perform action: @escaping ((IndexPath, UITableView) -> Void)) {
+        didLongPressRow = action
+    }
+    
+    func onDidCommitEditing(perform action: @escaping ((UITableViewCellEditingStyle, IndexPath, UITableView) -> Void)) {
+        didCommitEditing = action
+    }
+
+    private func configureNavigationItems() {
+        let addAction = #selector(didTap(addBarButtonItem:))
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: addAction)
+        navigationItem.leftBarButtonItem = addButton
+        navigationItem.rightBarButtonItem = editButtonItem
+        navigationItem.leftBarButtonItem?.accessibilityLabel = "Add Item"
+        navigationItem.title = "All Items"
     }
 
     private func configureTableView() {
@@ -47,42 +83,25 @@ final class TableViewController: UIViewController {
         tableView.delegate = self
     }
 
+    private func didSetViewModel() {
+        tableView.reloadData()
+        editButtonItem.isEnabled = viewModel.isEditButtonEnabled
+    }
+
+    @objc private func didTap(addBarButtonItem: UIBarButtonItem) {
+        self.didTap?(addBarButtonItem)
+    }
+
     @objc private func didLongPress(sender: UILongPressGestureRecognizer) {
         let point = sender.location(in: sender.view)
         let tableView = sender.view as? UITableView
         let indexPath = tableView?.indexPathForRow(at: point)
 
-        guard let index = indexPath, sender.state == .began else {
+        guard let table = tableView, let index = indexPath, sender.state == .began else {
             return
         }
 
-        viewModel.didLongPressRow(at: index)
-    }
-
-    private func configureViewModel() {
-        viewModel.isEditButtonEnabled = { [weak self] isEnabled in
-            self?.editButtonItem.isEnabled = isEnabled
-        }
-
-        viewModel.reloadRows = { [weak self] indexPaths, animationStyle in
-            self?.tableView.reloadRows(at: indexPaths, with: animationStyle)
-        }
-
-        viewModel.insertRows = { [weak self] indexPaths, animationStyle in
-            self?.tableView.insertRows(at: indexPaths, with: animationStyle)
-        }
-
-        viewModel.deleteRows = { [weak self] indexPaths, animationStyle in
-            self?.tableView.deleteRows(at: indexPaths, with: animationStyle)
-        }
-
-        viewModel.deselectRow = { [weak self] indexPath, animated in
-            self?.tableView.deselectRow(at: indexPath, animated: animated)
-        }
-
-        viewModel.setEditing = { [weak self] bool, animated in
-            self?.setEditing(bool, animated: animated)
-        }
+        didLongPressRow?(index, table)
     }
 }
 
@@ -110,11 +129,11 @@ extension TableViewController: UITableViewDataSource {
 extension TableViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        viewModel.didSelectRow(at: indexPath)
+        didSelectRow?(indexPath, tableView)
     }
-    
+
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        viewModel.commit(edit: editingStyle, forRowAt: indexPath)
+        didCommitEditing?(editingStyle, indexPath, tableView)
     }
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
