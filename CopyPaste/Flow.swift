78155374614906676beae48cb5c0a1de6847babd
Garric Nahapetian <garricn@icloud.com>
2017-11-24 17:42:33 -0800

    Introduce Flow protocol

---
+++ CopyPaste/Flow.swift
@@ -0,0 +1,19 @@
+//
+//  Flow.swift
+//  CopyPaste
+//
+//  Created by Garric G. Nahapetian on 11/24/17.
+//  Copyright © 2017 SwiftCoders. All rights reserved.
+//
+
+import UIKit
+
+protocol Flow {}
+
+extension Flow {
+    func add(_ child: UIViewController, to parent: UIViewController) {
+        parent.addChildViewController(child)
+        parent.view.addSubview(child.view)
+        child.didMove(toParentViewController: parent)
+    }
+}
