670876f8a443963cae00923a1120bcc7f4dd61ab
Garric Nahapetian <garricn@icloud.com>
2018-03-31 17:53:33 -0700

    Move Defaults to own file

---
+++ CopyPaste/Defaults.swift
@@ -0,0 +1,13 @@
+//
+//  Defaults.swift
+//  CopyPaste
+//
+//  Created by Garric Nahapetian on 3/31/18.
+//  Copyright © 2018 SwiftCoders. All rights reserved.
+//
+
+import Foundation
+
+public struct Defaults: Codable {
+    public var showWelcome = true
+}
