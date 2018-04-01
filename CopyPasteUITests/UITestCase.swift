b770d45537aaf76e0634c9a5783012e6f9c6de17
Garric Nahapetian <garricn@icloud.com>
2018-03-31 18:03:14 -0700

    Delete UITestHelper class

--- CopyPasteUITests/UITestCase.swift
+++ CopyPasteUITests/UITestCase.swift
@@ -25,35 +25,13 @@ class UITestCase: XCTestCase {
         let element = XCUIApplication().cells.staticTexts["Add Item"]
         assertApp(isDisplaying: element)
     }
-}
-
-public final class UITestHelper {
-    public static func debugPerform(resetAction: ResetAction, application: XCUIApplication) {
-        application.navigationBars["All Items"].buttons["debug"].tap()
-        application.sheets["RESET"].buttons[resetAction.rawValue].tap()
+    
     func setItems(to items: [Item]) {
         let data = try! JSONEncoder().encode(items)
         let string = String(data: data, encoding: .utf8)
         app.launchEnvironment[Globals.EnvironmentVariables.items] = string
     }
-    public enum ResetAction {
-        case data
-        case userDefaults
-        case dataAndUserDefaults
-        case cancel
-        
-        var rawValue: String {
-            switch self {
-            case .data:
-                return Globals.UITestingResetAction.data
-            case .userDefaults:
-                return Globals.UITestingResetAction.defaults
-            case .dataAndUserDefaults:
-                return Globals.UITestingResetAction.dataAndDefaults
-            case .cancel:
-                return Globals.UITestingResetAction.cancel
-            }
-        }
+    
     func setDefaults(to defaults: Defaults) {
         let data = try! JSONEncoder().encode(defaults)
         let string = String(data: data, encoding: .utf8)
