509142714e7ec05d3c848c67440502a756ff8993
Garric Nahapetian <garricn@icloud.com>
2018-04-08 16:03:40 -0700

    Remove typealiases as unneeded

--- CopyPaste/AppDelegate.swift
+++ CopyPaste/AppDelegate.swift
@@ -11,15 +11,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     private lazy var appFlow: AppFlow = AppFlow(window: self.window)
 
-    typealias App = UIApplication
-    typealias Options = [UIApplicationLaunchOptionsKey: Any]
-    func application(_ application: App, didFinishLaunchingWithOptions launchOptions: Options?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return appFlow.didFinish(.init(options: launchOptions))
     }
 
-    typealias Item = UIApplicationShortcutItem
-    typealias Handler = (Bool) -> Void
-    func application(_ application: App, performActionFor shortcutItem: Item, completionHandler: @escaping Handler) {
+    func application(_ application: UIApplication,
+                     performActionFor shortcutItem: UIApplicationShortcutItem,
+                     completionHandler: @escaping (Bool) -> Void) {
         appFlow.performAction(for: shortcutItem, completion: completionHandler)
     }
 }
